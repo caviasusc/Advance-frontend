302db48483ed1c1bced020b96ea8d85fdef7ac69
Cesar Viasus <caviasusc@gmail.com>
2021-02-08 14:41:44 -0500

    b

--- src/components/employee.js
+++ src/components/employee.js
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import {useForm} from 'react-hook-form'
 import { Button, TextField, InputLabel, Select, MenuItem, FormControl } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
+import NewEmployee from './newEmployee'
+import UpdateEmployee from './updateEmployee'
 
 
 const useStyles = makeStyles((theme) => ({
@@ -15,108 +18,6 @@ const useStyles = makeStyles((theme) => ({
         margin: 5,
     }
 }));
-
-const currencies = [
-    {
-        value: 1,
-        label: 'Hombre',
-    },
-    {
-        value: 2,
-        label: 'Mujer',
-    },
-    {
-        value: 3,
-        label: 'Otro',
-    },
-
-];
-
-function NewEmployee() {
-    const classes = useStyles();
-    const sumbit = async ()=>{
-        console.log('creado')
-    }
-    return (
-        <div>
-            <FormControl className={classes.formControl}>
-                <h3>Ingrese los datos del empleado</h3>
-                <TextField
-                    label="Nombre"
-                    placeholder="Nombre"
-                    id="name-TF"
-                    variant="outlined"
-                    required
-                />
-                <TextField
-                    label="Apellido"
-                    placeholder="Apellido"
-                    id="lastname-TF"
-                    variant="outlined"
-                    required
-                />
-                <TextField
-                    id="standard-select-currency"
-                    select
-                    label="Select"
-                    helperText="por favor selecione su genero"
-                    required
-                >
-                    {currencies.map((option) => (
-                        <MenuItem key={option.value}>
-                            {option.label}
-                        </MenuItem>
-                    ))}
-                </TextField>
-                <TextField
-                    label="Email"
-                    placeholder="Email"
-                    id="email-TF"
-                    variant="outlined"
-                    required
-                />
-                <TextField
-                    label="Dirección"
-                    placeholder="Dirección"
-                    id="name-TF"
-                    variant="outlined"
-                />
-                <TextField
-                    label="Teléfono"
-                    placeholder="Teléfono"
-                    id="lastname-TF"
-                    variant="outlined"
-                    required
-                />
-                <TextField
-                    label="Tipo de documento"
-                    placeholder="Tipo de documento"
-                    id="name-TF"
-                    variant="outlined"
-                    required
-                />
-                <TextField
-                    label="Documento"
-                    placeholder="Documento"
-                    id="lastname-TF"
-                    variant="outlined"
-                    required
-                />
-                <Button variant="outlined" color="primary" onClick={()=>sumbit()}>Crear</Button>
-            </FormControl>
-        </div>
-    );
-}
-function UpdateEmployee() {
-    return (
-        <div>
-            <form>
-                <h2>Ingrese datos del usuario</h2>
-                <h3>para editar</h3>
-            </form>
-        </div>
-    );
-}
 
 function Employee(props) {
     const show = props.show
---
+++ src/components/newEmployee.js
@@ -0,0 +1,189 @@
+import React, { useEffect, useState } from 'react';
+import { useForm } from 'react-hook-form'
+import { Button, TextField, InputLabel, Select, MenuItem, FormControl } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import Radio from '@material-ui/core/Radio';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormLabel from '@material-ui/core/FormLabel';
+import './employeeForm.css';
+
+
+function NewEmployee() {
+
+    const { register, errors, handleSubmit, reset, setValue, getValues } = useForm({
+        mode: 'onChange',
+        defaultValues: {
+            first_name: '',
+            last_name: '',
+            email: '',
+            address: '',
+            phone_number: '',
+            document_number: ''
+        },
+    });
+    const [gender, setGender] = useState();
+    const [docType, setDocType] = useState();
+    const values = getValues();
+    const handleChange = (event) => {
+        setGender(event.target.value);
+    };
+    const handleChangeDoc = (event) => {
+        setDocType(event.target.value);
+    };
+
+    const onSubmit = async (data) => {
+
+        let info = values
+        info.document_type = docType
+        info.gender = gender
+
+        var requestOptions = {
+            method: 'POST',
+            headers: {"Content-Type": "application/json"},
+            body: info,
+            redirect: 'follow'
+        };
+
+        await fetch("https://my-test-cv.herokuapp.com/employee/", requestOptions)
+            .then(response => response.text())
+            .then(result => console.log(result))
+            .catch(error => console.log('error', error));
+        reset();
+    };
+
+    return (
+        <div>
+            <h2>Nuevo empleado</h2>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <TextField
+                    label="Nombre"
+                    id="firstname"
+                    name="firstname"
+                    value={values.firstname}
+                    onChange={event => setValue('firstname', event.target.value, true)}
+                    required
+                    inputRef={
+                        register({
+                            required: { value: true, message: 'Ingrese un nombre' },
+                        })
+                    }
+                />
+                <br />
+                <TextField
+                    label="Apellido"
+                    id="lastname"
+                    name="lastname"
+                    required
+                    onChange={event => setValue('lastname', event.target.value, true)}
+                    inputRef={
+                        register({
+                            required: { value: true, message: 'Ingrese un nombre' },
+                        })
+                    }
+                />
+                <br />
+                <FormControl component="fieldset">
+                    <FormLabel component="legend">Género</FormLabel>
+                    <RadioGroup
+                        aria-label="gender"
+                        name="gender"
+                        value={values.gender}
+                        onChange={handleChange}
+                        required
+                        inputRef={
+                            register({
+                                required: { value: true, message: 'Ingrese un nombre' },
+                            })
+                        }
+                    >
+                        <FormControlLabel value="Mujer" control={<Radio />} label="Mujer" />
+                        <FormControlLabel value="Hombre" control={<Radio />} label="Hombre" />
+                        <FormControlLabel value="Otro" control={<Radio />} label="Otro" />
+                    </RadioGroup>
+                </FormControl>
+                <br />
+                <TextField
+                    label="Email"
+                    placeholder="Email"
+                    id="email-TF"
+                    name="email"
+                    onChange={event => setValue('email', event.target.value, true)}
+                    required
+                    inputRef={
+                        register({
+                            required: { value: true, message: 'Ingrese un nombre' },
+                            pattern: {
+                                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                                message: "Correo invalido!"
+                            }
+                        })
+                    }
+                /> <br /><span className="error">{errors?.email?.message}</span>
+                <br />
+                <TextField
+                    label="Dirección"
+                    placeholder="Dirección"
+                    name="address"
+                    id="address"
+                    onChange={event => setValue('address', event.target.value, true)}
+                    inputRef={
+                        register({
+                            required: { value: true, message: 'Ingrese un nombre' },
+                        })
+                    }
+                /><br />
+                <TextField
+                    label="Teléfono"
+                    placeholder="Teléfono"
+                    id="phone-TF"
+                    name="phone_number"
+                    required
+                    onChange={event => setValue('phone_number', event.target.value, true)}
+                    inputRef={
+                        register({
+                            required: { value: true, message: 'Ingrese un nombre' },
+                        })
+                    } />
+                <br />
+               <FormControl component="fieldset">
+                    <FormLabel component="legend">Tipo de documento</FormLabel>
+                    <RadioGroup
+                        aria-label="docType"
+                        name="docType"
+                        value={values.docType}
+                        onChange={handleChangeDoc}
+                        required
+                        inputRef={
+                            register({
+                                required: { value: true, message: 'Ingrese un nombre' },
+                            })
+                        }
+                    >
+                        <FormControlLabel value="Cedula" control={<Radio />} label="Cedula" />
+                        <FormControlLabel value="Cedula de etranjeria" control={<Radio />} label="Cedula de etranjeria" />
+                        <FormControlLabel value="Otro" control={<Radio />} label="Otro" />
+                    </RadioGroup>
+                </FormControl><br />
+                <TextField
+                    label="Documento"
+                    placeholder="Documento"
+                    id="document_number"
+                    name="document_number"
+                    required
+                    onChange={event => setValue('document_number', event.target.value, true)}
+                    inputRef={
+                        register({
+                            required: { value: true, message: 'Ingrese un nombre' },
+                        })
+                    }
+                />
+                <br />
+                <input className="subButton" type="submit"></input>
+            </form>
+        </div >
+    );
+
+}
+
+export default NewEmployee
